3023b366f1e434964fcbd43508147580fcd16379
Shawn Lu <42988703+chees-e@users.noreply.github.com>
2019-06-24 13:20:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config;
 const fs = require('fs');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+
 bot.commands = new Discord.Collection();
+
 
 fs.readdir('./commands/', (err, files) => {
   if(err) console.log(err);
@@ -34,6 +36,12 @@ bot.on('guildCreate', (guild) => {
   console.log(`bot has been added to ${guild.name}, (ID: ${guild.id})`);
 });
 
+bot.on('guildDelete', (guild) => {
+  console.log(`bot has been removed from ${guild.name}, (ID: ${guild.id})`);
+});
 
+bot.on('message`, async (message) => {
+       
+});
 
 
