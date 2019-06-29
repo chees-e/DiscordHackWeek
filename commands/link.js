134939dfca507fb7c9e409f45b0a4b252c384034
Shawn Lu <42988703+chees-e@users.noreply.github.com>
2019-06-28 18:05:51 -0700

    Update link.js

--- commands/link.js
+++ commands/link.js
@@ -1,1 +1,9 @@
+module.exports.run = async(bot, message, args, prefix)=>{
+  await message.channel.send("Here is the authorization link:")
+  return message.channel.send('```\nhttps://discordapp.com/api/oauth2/authorize?client_id=466445188437377024&permissions=335801392&scope=bot\n```')
+}
 
+module.exports.help = {
+  name: "link",
+  description: `This will return the invitation link for this bot`
+}
